24700c458f54d46c3f1025140a8802171aac4b22
Asif <sheikasifofficial@gmail.com>
2022-11-03 20:55:15 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftyUserInterface
+
+A description of this package.
---
+++ Tests/SwiftyUserInterfaceTests/SwiftyUserInterfaceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyUserInterface
+
+final class SwiftyUserInterfaceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyUserInterface().text, "Hello, World!")
+    }
+}
